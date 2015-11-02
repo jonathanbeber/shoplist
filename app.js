b5a68d91b9aa75272878bba72afcfd4c95a39d1a
Jonathan Juares Beber <jonathan.beber@contaazul.com>
2015-11-02 19:47:28 +0000

    Port fix

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(8080);
+app.listen( process.env.VCAP_APP_PORT || 8080 );
