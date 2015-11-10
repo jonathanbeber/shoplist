be26ced7774957ece2dc0fe2f624b47577cfc58c
Jonathan Juares Beber <jonathan.beber@contaazul.com>
2015-11-10 04:30:34 +0000

    fim do update

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var routes = require('./routes');
 var ejs = require('ejs');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-/* app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
-})); */
+}));
 app.get('/', routes.index);
 app.get('/lista', routes.lista);
 app.post('/add', routes.add);
+app.put('/update', routes.update);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
