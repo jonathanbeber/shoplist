29f08cda01a7ad8da5ea36046f61ae2a190f8067
Jonathan Juares Beber <jonathan.beber@contaazul.com>
2015-11-10 04:19:47 +0000

    Adicionado update na rota

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,21 @@ exports.lista = function(req, res) {
 }
 exports.add = function(req, res) {
 	var item = new Item(req.body);
-	
 	item.save(
 		function(err, item){
 			if(err) return console.log('Erro ao salvar');
+			res.send(item);
+		}
+	);
+}
+exports.update = function(req, res) {
+	var id = req.body._id;
+	delete req.body._id;
+	Item.findByIdAndUpdate(
+		id,
+		req.body,
+		function(err, item){
+			if(err) return console.log('Erro ao fazer update: ' + err);
 			res.send(item);
 		}
 	);
